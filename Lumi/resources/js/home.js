a50807c0da49cd73d7107818259f04d3b9683201
OptimumRaZe <razemonster@outlook.com>
2021-10-18 17:31:57 -0600

    landing page w js

---
+++ Lumi/resources/js/home.js
@@ -0,0 +1,24 @@
+  function toggleTable() {
+    var lTable = document.getElementById("weather-table");
+    lTable.style.display = (lTable.style.display == "table") ? "none" : "table";
+  }
+  function show_hide_column() {
+    var table = document.getElementById("weather-table");
+    for (var i = 0, row; row = table.rows[i]; i++) {
+      for (var j = 0, col; col = row.cells[j]; j++) {
+        if (j==4)
+        {
+          col.style.display="none";
+        }
+      }
+    }
+    element.style.display = "none";
+  }
+  function isUser()
+  {
+    var user=true;
+    if (user!=true)
+    {
+      show_hide_column();
+    }
+  }
